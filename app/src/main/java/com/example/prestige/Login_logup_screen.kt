4da9a6ab1bbd600527b7bf39540c20f4df7dd52b
Ujjwal sai <ujjwalsai501@gmail.com>
2024-12-26 12:48:31 +0530

    Added first launching screen

---
+++ app/src/main/java/com/example/prestige/Login_logup_screen.kt
@@ -0,0 +1,20 @@
+package com.example.prestige
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Login_logup_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login_logup_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
