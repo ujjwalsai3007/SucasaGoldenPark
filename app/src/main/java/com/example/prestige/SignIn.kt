2ecfca09744f532da9108ab5bc60b92f0e69c91f
Ujjwal sai <ujjwalsai501@gmail.com>
2024-12-27 14:50:02 +0530

    Added SignUp features with firebase and validations

--- app/src/main/java/com/example/prestige/SignIn.kt
+++ app/src/main/java/com/example/prestige/SignIn.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Login_logup_screen : AppCompatActivity() {
+class SignIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login_logup_screen)
+        setContentView(R.layout.activity_sign_in)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
